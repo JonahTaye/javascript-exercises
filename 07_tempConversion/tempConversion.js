41e3d7c4f304f978073087660949d9a55a396d79
jonah <jonah.softwaredev@gmail.com>
2025-01-30 16:32:27 +0300

    feat: add temp converter func for celcius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * 5/9
+  
+  return Math.round(celcius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius * 9/5) + 32
+  
+  return Math.round(fahrenheit * 10) / 10
 };
-
+console.log(convertToCelsius(32))
+console.log(convertToFahrenheit(0))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
