16d608dc1d2f46fa6831f8622e8f221aad5ea9bc
jonah <jonah.softwaredev@gmail.com>
2025-01-30 11:00:46 +0300

    feat: add repeatString() func to repeat string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let append = string;
 
+    if (!times) return string = ''
+    else if (times < 0) return 'ERROR'
+
+    for (let i = 1; i < times; i++) {
+        string += append
+    }
+
+    return string
 };
 
 // Do not edit below this line
