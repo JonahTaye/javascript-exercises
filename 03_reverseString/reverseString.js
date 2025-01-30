e79170208dd2c555f4109ce9988e887d6305d436
jonah <jonah.softwaredev@gmail.com>
2025-01-30 12:36:56 +0300

    feat: add reverseString() func to reverse string order

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverseText = []
+    let j = 0
+    text = text.split(" ").reverse()
 
+    for (let word of text) {
+        reverseText[j] = ""
+        for (let i = word.length - 1; i >= 0; i--) {
+            reverseText[j] += word[i]
+        }
+        j += 1
+    }
+
+    return reverseText.join(" ").trim()
 };
 
 // Do not edit below this line
