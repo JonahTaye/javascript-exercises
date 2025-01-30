cdf28f1388d009f5af5b0a2b7183d8fe6704255d
jonah <jonah.softwaredev@gmail.com>
2025-01-30 13:49:34 +0300

    feat: add removeFromArray() func for removing items in array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...extraItems) {
+    let newArray = originalArray.filter(removeItem)
+
+    function removeItem(item) {
+        if (!(extraItems.includes(item))) {
+            return item
+        }
+    }
+
+    return newArray
 };
 
+removeFromArray([1,3,3,4, 5, 6],3, 4, 5)
 // Do not edit below this line
 module.exports = removeFromArray;
