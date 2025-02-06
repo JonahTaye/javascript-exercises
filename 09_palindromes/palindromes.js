a8caa58fa95475ab6888b71d0d294e4df7bb0ace
jonah <jonah.softwaredev@gmail.com>
2025-02-06 15:24:42 +0300

    feat: add palindromes() function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let specialChar = [" ", ".", ",","!"]
+    let isPalindrome = false
+    
+    const splitString = string.toLowerCase().split("")
+    const filteredString = splitString.filter(char => !specialChar.includes(char))
+    const reversedString = filteredString.toReversed()
+    
+    for (let i = 0; i <= filteredString.length; i++) {
+        if (filteredString[i] === reversedString[i]) isPalindrome = true
+        else return false
+    }
+    
+    return isPalindrome
 };
-
+console.log(palindromes('A car, a man, a maraca.'))
 // Do not edit below this line
 module.exports = palindromes;
