27f0c137101ce3025cea462999d253236ec26a67
jonah <jonah.softwaredev@gmail.com>
2025-02-06 17:01:13 +0300

    feat: add findTheOldest() func

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentDate = new Date().getFullYear()
 
+    people.sort((personA, personB) => {        
+        if (!personA.yearOfDeath) personA["yearOfDeath"] = currentDate
+        else if (!personB.yearOfDeath) personB["yearOfDeath"] = currentDate
+       
+        ageA = personA.yearOfDeath - personA.yearOfBirth
+        ageB = personB.yearOfDeath - personB.yearOfBirth
+
+        return ageB - ageA
+    })
+
+    return people[0]
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+console.log(findTheOldest(people).name)
 
 // Do not edit below this line
 module.exports = findTheOldest;
