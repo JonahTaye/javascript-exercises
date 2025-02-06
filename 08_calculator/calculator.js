ece5e1bc8b2e3167fd7765e50b0ef68a67ef2438
jonah <jonah.softwaredev@gmail.com>
2025-02-06 14:42:58 +0300

    feat: add calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return parseInt(a) + parseInt(b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return parseInt(a) - parseInt(b)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => total + number, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, number) => product * number, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+  let total = 1
+  
+	for (let i = 1; i <= number; i++) {
+    total *= i
+  }
 
+  return total
+};
+console.log(factorial(2))
 // Do not edit below this line
 module.exports = {
   add,
