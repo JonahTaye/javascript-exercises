8ce2197f16b79bbb45095a1468c7ecfab210923f
jonah <jonah.softwaredev@gmail.com>
2025-02-06 16:04:11 +0300

    feat: add fibonacci() func

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fibonacciSequence = [0, 1]
 
+    if (n < 0) return "OOPS"
+
+    while (fibonacciSequence.length <= n) {
+        fibonacciSequence.push(fibonacciSequence.at(-2) + fibonacciSequence.at(-1))
+    }
+    return fibonacciSequence[n]
 };
-
+console.log(fibonacci(6))
 // Do not edit below this line
 module.exports = fibonacci;
